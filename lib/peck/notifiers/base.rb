2f3d65e7b4b8fe0b646623213a34164839b397f8
Manfred Stienstra <manfred@fngtps.com>
2012-09-03 10:38:34 +0200

    Move backtrace cleaning to a module.
    By moving the backtrace cleaning into a module and including it into the
    notifier base we can include modules to add additional cleaning in
    extension libraries.

--- lib/peck/notifiers/base.rb
+++ lib/peck/notifiers/base.rb
@@ -1,11 +1,26 @@
 class Peck
   class Notifiers
     class Base
-      # When a file starts with this path, it's in the Peck library
-      PECK_PATH = File.expand_path('../../../../', __FILE__)
+      module BacktraceCleaning
+        # When a file starts with this path, it's in the Peck library
+        PECK_PATH = File.expand_path('../../../../', __FILE__)
 
-      # Matches: `block (2 levels) in <top (required)>'
-      ANONYMOUS_BLOCK_RE = /`block/
+        # Matches: `block (2 levels) in <top (required)>'
+        ANONYMOUS_BLOCK_RE = /`block/
+
+        def clean_backtrace(backtrace)
+          stripped = []
+          backtrace.each do |line|
+            if line.start_with?(PECK_PATH) || line.start_with?("<")
+            elsif line =~ ANONYMOUS_BLOCK_RE
+              stripped << line.split(':')[0,2].join(':')
+            else
+              stripped << line
+            end
+          end
+          stripped.empty? ? backtrace : stripped
+        end
+      end
 
       def self.use
         @instance ||= begin
@@ -22,18 +37,7 @@ class Peck
         count == 1 ? stem : "#{stem}s"
       end
 
-      def clean_backtrace(backtrace)
-        stripped = []
-        backtrace.each do |line|
-          if line.start_with?(PECK_PATH) || line.start_with?("<")
-          elsif line =~ ANONYMOUS_BLOCK_RE
-            stripped << line.split(':')[0,2].join(':')
-          else
-            stripped << line
-          end
-        end
-        stripped.empty? ? backtrace : stripped
-      end
+      include BacktraceCleaning
     end
   end
 end
