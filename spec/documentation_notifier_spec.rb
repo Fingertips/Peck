6770fc5c29e9c85d54cbe3cbe829e443e8265f5d
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2013-08-09 17:15:18 +0200

    Add documentation_notifier_spec.rb

---
+++ spec/documentation_notifier_spec.rb
@@ -0,0 +1,69 @@
+# encoding: utf-8
+
+require File.expand_path('../preamble', __FILE__)
+require 'stringio'
+
+class FakeSpec < Struct.new(:label)
+end
+
+describe Peck::Notifiers::Documentation do
+  before do
+    @notifier = Peck::Notifiers::Documentation.new
+  end
+
+  it "formats test failures into a readable format" do
+    exception = nil
+    begin
+      raise ArgumentError, "Is a good example of what might happen"
+    rescue => e
+      exception = e
+    end
+
+    spec = FakeSpec.new("Event should go on")
+    event = Peck::Event.new(exception, spec)
+
+    capture_stdout do
+      @notifier.write_event(2, event)
+    end.should == "  2) Event should go on
+
+  Is a good example of what might happen
+
+\tspec/documentation_notifier_spec.rb:17
+
+"
+  end
+
+  it "formats test failures without a message" do
+    exception = nil
+    begin
+      fail
+    rescue => e
+      exception = e
+    end
+
+    spec = FakeSpec.new("Event should go on")
+    event = Peck::Event.new(exception, spec)
+
+    capture_stdout do
+      @notifier.write_event(2, event)
+    end.should == "  2) Event should go on
+
+\tspec/documentation_notifier_spec.rb:39
+
+"
+  end
+
+  private
+
+  def capture_stdout
+    stdout = $stdout
+    $stdout = written = StringIO.new('')
+    begin
+      yield
+    ensure
+      $stdout = stdout
+    end
+    written.rewind
+    written.read
+  end
+end
