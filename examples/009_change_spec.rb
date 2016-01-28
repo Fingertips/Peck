70261fefad143a631cfdadeb651201f3b387bc6d
Steven Weller <github@suweller.com>
2016-01-28 21:34:10 +0100

    Allow #change work without active support core-ext

---
+++ examples/009_change_spec.rb
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+require 'preamble'
+
+describe 'change' do
+  it 'tracks a single change' do
+    a = 1
+    lambda do
+      a += 1
+    end.should.change('a', +1)
+  end
+
+  it 'tracks multiple changes' do
+    a = 1
+    b = 4
+    lambda do
+      a += 1
+      b -= 1
+    end.should.change('a', +1, 'b', -1)
+  end
+end
+
+# We need to specs to run now, not at exit because we want to inspect
+# the outcomes.
+
+Peck.run
+
+require 'assert'
+
+assert(Peck.counter.ran == 2,
+  "Expected five specification to have been run")
+
+assert(Peck.counter.passed == 2,
+  "Expected five specification to have passed")
+
