b77b97a636f1bd70e000e11ef8798ff22170e44b
Manfred Stienstra <manfred@fngtps.com>
2016-07-27 14:31:01 +0200

    Remove superfluous value from character range.

--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -4,7 +4,7 @@ require File.expand_path('../preamble', __FILE__)
 
 describe "The context" do
   it "returns a readable inspect" do
-    self.inspect.should =~ /#<Peck::Context:0x[\w\d]+ @description="The context">/
+    self.inspect.should =~ /#<Peck::Context:0x[\w]+ @description="The context">/
     self.class.inspect.should == '#<Peck::Context description="The context…">'
   end
 end
